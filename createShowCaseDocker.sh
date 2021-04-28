7a6012af9f1972e26d13fd655b424edef148657d
angelo lazzaretti <angelo.lazzaretti@gmail.com>
2021-04-28 13:26:10 +0200

    agginte descrizioni dei servizi

--- createShowCaseDocker.sh
+++ createShowCaseDocker.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+echo " BUILD IMAGE SHOWCASE-6.2"
 sudo docker build -t showcase:1.0 .
+echo " RUN IMAGE SHOWCASE-6.2"
 sudo docker run -d --name showcase --hostname showcase -p 8443:8443 showcase:1.0
+echo ""
+echo "https://localhost:8443/prime-showcase/"
